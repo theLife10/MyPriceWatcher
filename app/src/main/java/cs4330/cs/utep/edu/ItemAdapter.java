582c7930da2562df537f2c6e7598310a12b3f3ea
theLife10 <jdgarcia9@miners.utep.edu>
2019-03-05 15:09:12 -0700

    figured out the adapter. remaking the views now.

--- app/src/main/java/cs4330/cs/utep/edu/ItemAdapter.java
+++ app/src/main/java/cs4330/cs/utep/edu/ItemAdapter.java
@@ -1,13 +1,12 @@
 package cs4330.cs.utep.edu;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.NonNull;
 
 
 import java.util.ArrayList;
@@ -17,29 +16,47 @@ public class ItemAdapter extends ArrayAdapter<Item> {
 
     private Context mContext;
     private List<Item> itemList = new ArrayList<>();
-    private Item item;
-    TextView itemName;
+    TextView itemName, currentPrice, startingPrice,priceChange;
     public ItemAdapter( Context context, List<Item> items){
         super(context, 0, items);
         mContext = context;
         itemList = items;
     }
-
     @NonNull
     @Override
     public View getView(int position,View convertView,ViewGroup parent){
         View listItem = convertView;
-        if(listItem == null)
+        if(listItem == null){
             listItem = LayoutInflater.from(mContext).inflate(R.layout.listview_row,parent,false);
+        }
+
+        Item item = itemList.get(position);
+        item.setStartPrice();
 
         //setting item name
         itemName = (TextView) listItem.findViewById(R.id.itemName);
         itemName.setText(item.getItem());
 
+        startingPrice = (TextView) listItem.findViewById(R.id.startingPrice);
+        startingPrice.setText(Double.toString(item.getStartPrice()));
+
+        currentPrice = (TextView) listItem.findViewById(R.id.currentPrice);
+        currentPrice.setText(Double.toString(item.getCurrentPrice()));
+
         //init  startingPrice, currentPrice, and priceChange
-       TextView startingPrice = (TextView) listItem.findViewById(R.id.startingPrice);
-       TextView currentPrice = (TextView) listItem.findViewById(R.id.currentPrice);
-       TextView priceChange = (TextView) listItem.findViewById(R.id.percent);
-       return  convertView;
+
+
+       // priceChange = (TextView) listItem.findViewById(R.id.percent);
+
+
+       // settingNumbers();
+       return  listItem;
     }
+
+   // public void settingNumbers(){
+      //  item.setCurrentPrice();
+//        format the strings
+        //currentPrice.setText(Float.toString(item.getCurrentPrice()));
+      //  priceChange.setText(Float.toString(item.getPercentageChange()));
+   // }
 }
--- app/src/main/java/cs4330/cs/utep/edu/MainActivity.java
+++ app/src/main/java/cs4330/cs/utep/edu/MainActivity.java
@@ -1,61 +1,59 @@
 package cs4330.cs.utep.edu;
 
-import android.content.Intent;
-import android.net.Uri;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    TextView itemName,currentPrice,startingPrice,priceChange,linkPassed;
-    Button calButton;
     private ListView listview;
-    private ItemAdapter itemadapter;
-    Item item = new Item();
+    ItemAdapter adapter;
+   // Button calButton;
+   // private ListView listview;
+
+    //Item item = new Item();
     String url ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        ArrayList<Item> items = new ArrayList<>();
+
         listview = (ListView) findViewById(R.id.listview);
 
+        items.add(new Item());
+        items.add(new Item());
+        //calButton = (Button) findViewById(R.id.cal);
 
-        calButton = (Button) findViewById(R.id.cal);
 
+//        calButton.setOnClickListener(v -> {
+            //settingNumbers();
+  //      });
 
-        item.setStartPrice();
-        double start = item.getStartPrice();
-        startingPrice.setText(Double.toString(start));
+        //passLine();
 
-        calButton.setOnClickListener(v -> {
-            settingNumbers();
-        });
-
-        passLine();
+        adapter = new ItemAdapter(this,items);
+        listview.setAdapter(adapter);
 
     }
 
-    private void passLine() {
+   // private void passLine() {
         //checking if link passed
-        linkPassed = (TextView) findViewById(R.id.link);
-        url= "https://www.bestbuy.com/site/hp-15-6-laptop-amd-a6-series-4gb-memory-amd-radeon-r4-1tb-hard-drive-hp-finish-in-jet-black-with-a-maglia-texture/6240847.p?skuId=6240847&ref=212&loc=1&extStoreId=829&&ref=212&loc=DWA&gclid=CjwKCAiAkrTjBRAoEiwAXpf9CdXpa-p-0dHJhsHKlwDe4ak88E_oh7RZ1hdiAvokxuCcUKe9vofArRoCQYAQAvD_BwE&gclsrc=aw.ds";
-        linkPassed.setText(url);
-        String action = getIntent().getAction();
-        String type =getIntent().getType();
-        if(Intent.ACTION_SEND.equalsIgnoreCase(action) && type != null && ("text/plain".equals(type))) {
-            url = getIntent().getStringExtra(Intent.EXTRA_TEXT);
-            linkPassed.setText(url);
-        }
-    }
-
-    public void settingNumbers(){
-        item.setCurrentPrice();
-        //format the strings
-        currentPrice.setText(Float.toString(item.getCurrentPrice()));
-        priceChange.setText(Float.toString(item.getPercentageChange()));
-    }
+       // linkPassed = (TextView) findViewById(R.id.link);
+     //   url= "https://www.bestbuy.com/site/hp-15-6-laptop-amd-a6-series-4gb-memory-amd-radeon-r4-1tb-hard-drive-hp-finish-in-jet-black-with-a-maglia-texture/6240847.p?skuId=6240847&ref=212&loc=1&extStoreId=829&&ref=212&loc=DWA&gclid=CjwKCAiAkrTjBRAoEiwAXpf9CdXpa-p-0dHJhsHKlwDe4ak88E_oh7RZ1hdiAvokxuCcUKe9vofArRoCQYAQAvD_BwE&gclsrc=aw.ds";
+       // linkPassed.setText(url);
+       // String action = getIntent().getAction();
+        //String type =getIntent().getType();
+        //if(Intent.ACTION_SEND.equalsIgnoreCase(action) && type != null && ("text/plain".equals(type))) {
+          //  url = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+           // linkPassed.setText(url);
+        //}
+    //}
 }
